226c77c6dffdb7c7a92ee86eab3922fa766faddd
Rolan Burykin <r.burykin@levi9.com>
2013-04-16 18:00:40 +0300

    Added Contact entity.

---
+++ src/main/java/com/rolan/examples/spring/crudexample/ContactsDao.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.rolan.examples.spring.crudexample;
+
+/**
+ *
+ * @author Rolan Burykin
+ */
+public class ContactsDao {
+    
+}
---
+++ src/main/java/com/rolan/examples/spring/crudexample/dao/Contact.java
@@ -0,0 +1,99 @@
+package com.rolan.examples.spring.crudexample.dao;
+
+import java.util.Date;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
+@Entity
+@Table(name = "CONTACTS")
+public class Contact {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    @Column
+    private String name;
+    @Column
+    private String address;
+    @Column
+    private String gender;
+    @Column
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date dob;
+    @Column
+    private String email;
+    @Column
+    private String mobile;
+    @Column
+    private String phone;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public Date getDob() {
+        return dob;
+    }
+
+    public void setDob(Date dob) {
+        this.dob = dob;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getMobile() {
+        return mobile;
+    }
+
+    public void setMobile(String mobile) {
+        this.mobile = mobile;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+}
