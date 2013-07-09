029a1b0c0b19c5af10a8b531985eea92dd33bc17
Rol2772 <test>
2013-07-09 22:35:29 +0300

    Finally controller tests are working !!!

--- src/test/java/com/rolan/examples/spring/crudexample/controller/TestContactsController.java
+++ src/test/java/com/rolan/examples/spring/crudexample/controller/TestContactsController.java
@@ -1,8 +1,10 @@
 package com.rolan.examples.spring.crudexample.controller;
 
+import com.rolan.examples.spring.crudexample.controller.TestContactsController.ContactsControllerTestConfiguration;
 import com.rolan.examples.spring.crudexample.dao.ContactsDao;
 import com.rolan.examples.spring.crudexample.entity.Contact;
 import java.util.Collections;
+import java.util.List;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -17,35 +19,58 @@ import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 import static org.mockito.Mockito.*;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import static org.hamcrest.Matchers.*;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration
 public class TestContactsController {
-
+    
     @Autowired
     private ContactsDao contactsDao;
     @Autowired
     private ContactsController contactsController;
     private MockMvc mockMvc;
-
+    
     @Before
     public void setup() {
         mockMvc = MockMvcBuilders.standaloneSetup(contactsController).build();
     }
-
+    
     @Test
-    public void testGetAllContacts() throws Exception {
-        mockMvc.perform(post("/")).andExpect(status().isOk());
+    public void testDefaultPage() throws Exception {
+        List<Contact> contactsList = Collections.<Contact>emptyList();
+        when(contactsDao.getAllContacts()).thenReturn(contactsList);
+        mockMvc.perform(post("/")).andExpect(status().isOk()).
+                andExpect(view().name("/contacts/showContacts")).
+                andExpect(request().attribute("contacts", contactsList));
     }
-
+    
+    @Test
+    public void testViewAllContacts() throws Exception {
+        List<Contact> contactsList = Collections.<Contact>emptyList();
+        when(contactsDao.getAllContacts()).thenReturn(contactsList);
+        mockMvc.perform(post("/")).andExpect(status().isOk()).
+                andExpect(view().name("/contacts/showContacts")).
+                andExpect(request().attribute("contacts", contactsList));
+    }
+    
+    @Test
+    public void testCreateContactDetail() throws Exception {
+        mockMvc.perform(get("/contacts/contactDetail")).
+                andExpect(status().isOk()).
+                andExpect(view().name("contacts/contactDetail")).
+                andExpect(request().attribute("contact", notNullValue()));
+    }
+    
     @Configuration
     static class ContactsControllerTestConfiguration {
-
+        
         @Bean
         public ContactsDao contactsDao() {
             return mock(ContactsDao.class);
         }
-
+        
         @Bean
         ContactsController contactsController() throws NoSuchFieldException {
             return new ContactsController();
