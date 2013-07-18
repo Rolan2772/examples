607425704ae9e26be06ee9cee64d5cf9057452ec
Rol2772 <test>
2013-07-18 22:07:38 +0300

    Implemented DAO test with embedded H2 DB.

--- src/test/java/com/rolan/examples/spring/crudexample/dao/TestContactsDao.java
+++ src/test/java/com/rolan/examples/spring/crudexample/dao/TestContactsDao.java
@@ -1,23 +1,25 @@
 package com.rolan.examples.spring.crudexample.dao;
 
+import com.rolan.examples.spring.crudexample.entity.Contact;
 import java.util.Properties;
 import javax.sql.DataSource;
-import org.junit.After;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
-import org.springframework.jdbc.datasource.DriverManagerDataSource;
-import org.springframework.jdbc.datasource.embedded.EmbeddedDatabase;
+import org.springframework.core.env.Environment;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
 import org.springframework.orm.hibernate4.HibernateTransactionManager;
 import org.springframework.orm.hibernate4.LocalSessionFactoryBean;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+import static org.junit.Assert.*;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration
@@ -25,23 +27,19 @@ public class TestContactsDao {
 
     @Autowired
     private ContactsDao contactsDao;
-    @Autowired
-    private EmbeddedDatabase db;
 
     @Configuration
     @PropertySource("classpath:/dao.properties")
     @EnableTransactionManagement
     static class ContactsDaoTestConfiguration {
-        
-        @Value("${jdbc.url}") 
-        private String dataBaseUrl;
+
+        @Autowired
+        private Environment env;
 
         @Bean
-        public EmbeddedDatabase dataSource1() {
-            /*EmbeddedDatabaseBuilder builder = new EmbeddedDatabaseBuilder();
-             return builder.setType(EmbeddedDatabaseType.H2).build();
-             */
-            return null;
+        public DataSource dataSource() {
+            EmbeddedDatabaseBuilder builder = new EmbeddedDatabaseBuilder();
+            return builder.setType(EmbeddedDatabaseType.H2).build();
         }
 
         @Bean
@@ -54,20 +52,12 @@ public class TestContactsDao {
             return new SimpleContactsDao();
         }
 
-        @Bean
-        public DataSource dataSource() {
-            DriverManagerDataSource ds = new DriverManagerDataSource();
-            ds.setDriverClassName("com.mysql.jdbc.Driver");
-            ds.setUsername("crud_test");
-            ds.setPassword("111111");
-            ds.setUrl(dataBaseUrl);
-            return ds;
-        }
-
-        private Properties hibernateProperties() {
+        private Properties getHibernateProperties() {
             Properties properties = new Properties();
-            properties.setProperty("hibernate.dialect", "org.hibernate.dialect.MySQLDialect");
-            properties.setProperty("hibernate.show_sql", "true");
+            properties.setProperty("hibernate.dialect", env.getProperty("hibernate.dialect"));
+            properties.setProperty("hibernate.show_sql", env.getProperty("hibernate.show_sql"));
+            properties.setProperty("hibernate.hbm2ddl.import_files", env.getProperty("hibernate.hbm2ddl.import_files"));
+            properties.setProperty("hibernate.hbm2ddl.auto", env.getProperty("hibernate.hbm2ddl.auto"));
             return properties;
         }
 
@@ -75,9 +65,8 @@ public class TestContactsDao {
         public LocalSessionFactoryBean sessionFactory() {
             LocalSessionFactoryBean sessionFactory = new LocalSessionFactoryBean();
             sessionFactory.setDataSource(dataSource());
-            sessionFactory.setPackagesToScan(new String[]{
-                "com.rolan.examples.spring.crudexample.entity"});
-            sessionFactory.setHibernateProperties(hibernateProperties());
+            sessionFactory.setPackagesToScan(new String[]{"com.rolan.examples.spring.crudexample.entity"});
+            sessionFactory.setHibernateProperties(getHibernateProperties());
             return sessionFactory;
         }
 
@@ -86,14 +75,12 @@ public class TestContactsDao {
             return new HibernateTransactionManager(sessionFactory().getObject());
         }
     }
-
+    
     @Test
     public void testGetContactById() {
-        contactsDao.getContactById(1L);
-    }
-
-    @After
-    public void shutDown() {
-        // db.shutdown();
+        Long id = 7L;
+        Contact contact = contactsDao.getContactById(id);
+        assertNotNull(contact);
+        assertEquals(id, contact.getId());
     }
 }
---
+++ src/test/resources/Contacts.sql
@@ -0,0 +1,1 @@
+INSERT INTO `contacts` (`id`, `name`, `address`, `gender`, `dob`, `email`, `mobile`, `phone`) VALUES (7,'11111111111111','asdf',NULL,NULL,NULL,NULL,NULL),(8,'q234','2345',NULL,NULL,NULL,NULL,NULL);
