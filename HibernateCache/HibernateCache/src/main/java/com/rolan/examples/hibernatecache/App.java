5fb529f71add71490949f52471df01930c1ff7d6
Rolan Burykin <r.burykin@levi9.com>
2013-09-18 18:37:49 +0300

    Hibernate cache project creation.

---
+++ HibernateCache/HibernateCache/src/main/java/com/rolan/examples/hibernatecache/App.java
@@ -0,0 +1,13 @@
+package com.rolan.examples.hibernatecache;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
