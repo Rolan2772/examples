2f3e920a42e16173a5a561b0f74fd04a708d959c
Rol2772 <test>
2013-10-07 22:02:13 +0300

    Hibernate transactions project example.

---
+++ HibernateTransactioons/src/main/java/com/rolan/examples/hibernatetransactioons/App.java
@@ -0,0 +1,13 @@
+package com.rolan.examples.hibernatetransactioons;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
