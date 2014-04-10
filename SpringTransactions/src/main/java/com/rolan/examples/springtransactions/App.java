9f29accb037c3082707b795331dd949f0189d24e
Rolan <rol@mail>
2014-04-10 08:16:57 +0300

    Updated transactions examples.

--- SpringTransactions/src/main/java/com/rolan/examples/springtransactions/App.java
+++ SpringTransactions/src/main/java/com/rolan/examples/springtransactions/App.java
@@ -1,6 +1,6 @@
 package com.rolan.examples.springtransactions;
 
-import com.rolan.examples.springtransactions.dao.impl.UserService;
+import com.rolan.examples.springtransactions.service.UserService;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
@@ -9,12 +9,28 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
  *
  */
 public class App {
+
+    private ApplicationContext ctx;
     
     public static void main(String[] args) {
-        ApplicationContext ctx = new ClassPathXmlApplicationContext("applicationContext.xml");
+        App app = new App();
+        app.testRequiresNew();
+    }
+
+    public App() {
+        ctx = new ClassPathXmlApplicationContext("applicationContext.xml");
         System.out.println("Hello Transactions :)");
-        
+    }
+
+    public void test() {
         UserService userService = ctx.getBean(UserService.class);
         System.out.println(userService.getUsers());
+        System.out.println("Creating user...");
+        userService.createUser("Rolan");
+    }
+
+    public void testRequiresNew() {
+        UserService userService = ctx.getBean(UserService.class);
+        userService.requiresNewTest();
     }
 }
